3e8bf4ec06d7cfeaba384968b495a400bec91d0a
palasinio <31258581+palasinio@users.noreply.github.com>
2026-03-22 02:56:31 +0100

    Use class-based custom HAP types for Homebridge 2

--- src/HomeKitTypes.js
+++ src/HomeKitTypes.js
@@ -1,37 +1,39 @@
 'use strict';
 
-const inherits = require('util').inherits;
-
 module.exports = {
   registerWith: function (hap) {
 
     const Characteristic = hap.Characteristic;
     const Service = hap.Service;
 
-    Characteristic.TelegramBotQuiet = function () {
-      Characteristic.call(this, 'Quiet', '9799244D-7E74-471F-B672-C41C262F7337');
+    class TelegramBotQuiet extends Characteristic {
+      constructor() {
+        super('Quiet', TelegramBotQuiet.UUID);
 
-      this.setProps({
-        format: Characteristic.Formats.BOOL,
-        perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE],
-      });
+        this.setProps({
+          format: Characteristic.Formats.BOOL,
+          perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE],
+        });
 
-      this.value = this.getDefaultValue();
-    };
-    inherits(Characteristic.TelegramBotQuiet, Characteristic);
-    Characteristic.TelegramBotQuiet.UUID = '9799244D-7E74-471F-B672-C41C262F7337';
+        this.value = this.getDefaultValue();
+      }
+    }
+    TelegramBotQuiet.UUID = '9799244D-7E74-471F-B672-C41C262F7337';
+    Characteristic.TelegramBotQuiet = TelegramBotQuiet;
 
-    Service.TelegramBot = function (displayName, subtype) {
-      Service.call(this, displayName, 'CA1172BF-8FB8-4F26-98E9-71EE92F7AF63', subtype);
+    class TelegramBot extends Service {
+      constructor(displayName, subtype) {
+        super(displayName, TelegramBot.UUID, subtype);
 
-      // Required Characteristics
-      this.addCharacteristic(Characteristic.TelegramBotQuiet);
+        // Required Characteristics
+        this.addCharacteristic(Characteristic.TelegramBotQuiet);
 
-      // Optional Characteristics
-      this.addOptionalCharacteristic(Characteristic.Name);
-      this.addOptionalCharacteristic(Characteristic.StatusActive);
-    };
-    inherits(Service.TelegramBot, Service);
-    Service.TelegramBot.UUID = 'CA1172BF-8FB8-4F26-98E9-71EE92F7AF63';
+        // Optional Characteristics
+        this.addOptionalCharacteristic(Characteristic.Name);
+        this.addOptionalCharacteristic(Characteristic.StatusActive);
+      }
+    }
+    TelegramBot.UUID = 'CA1172BF-8FB8-4F26-98E9-71EE92F7AF63';
+    Service.TelegramBot = TelegramBot;
   }
 };
--- src/SendCharacteristic.js
+++ src/SendCharacteristic.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const debug = require('debug')('Bot');
-const inherits = require('util').inherits;
 const clone = require('clone');
 
 module.exports = {
@@ -11,100 +10,104 @@ module.exports = {
     const uuid = hap.uuid;
 
 
-    Characteristic.SendCharacteristic = function (botName, displayName, notifications, bot) {
-      this.UUID = uuid.generate(`${botName}:${displayName}`);
-      Characteristic.call(this, displayName, this.UUID);
+    class SendCharacteristic extends Characteristic {
+      constructor(botName, displayName, notifications, bot) {
+        const characteristicUuid = uuid.generate(`${botName}:${displayName}`);
+        super(displayName, characteristicUuid);
+        this.UUID = characteristicUuid;
 
-      const props = {
-        format: Characteristic.Formats.BOOL,
-        perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY],
-      };
-      this.setProps(props);
+        const props = {
+          format: Characteristic.Formats.BOOL,
+          perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY],
+        };
+        this.setProps(props);
 
-      this.value = false;
+        this.value = false;
 
-      if (notifications instanceof Array) {
-        // Old style configuration
-        this._mode = undefined;
-        this._randomize = true;
-        this._notifications = notifications;
+        if (notifications instanceof Array) {
+          // Old style configuration
+          this._mode = undefined;
+          this._randomize = true;
+          this._notifications = notifications;
+        }
+        else {
+          // New style configuration
+          this._mode = notifications.mode;
+          this._randomize = Object.prototype.hasOwnProperty.call(notifications, 'randomize') ? notifications.randomize : true;
+          this._notifications = notifications.messages;
+        }
+
+        if (['Markdown', 'HTML', undefined].indexOf(this._mode) === -1) {
+          throw new Error('Invalid notification configuration. Please check your configuration of homebridge-telegram.');
+        }
+        if (!(this._notifications instanceof Array) || this._notifications.length === 0) {
+          throw new Error('Must specify at least one message for telegram notifications.');
+        }
+
+        this._activeNotifications = clone(this._notifications);
+        this._notificationIndex = 0;
+
+
+        this._bot = bot;
+
+        this.on('set', this._onSet.bind(this));
       }
-      else {
-        // New style configuration
-        this._mode = notifications.mode;
-        this._randomize = Object.prototype.hasOwnProperty.call(notifications, 'randomize') ? notifications.randomize : true;
-        this._notifications = notifications.messages;
+
+      _onSet(value, callback) {
+
+        const self = this;
+
+        if (value) {
+          this.pickMessage()
+            .then(message => {
+              return this._bot.send(message, this._mode);
+            })
+            .then(() => {
+              callback();
+            })
+            .catch(e => {
+              callback(e);
+            })
+            .then(() => {
+              setTimeout(() => {
+                debug('[%s] Reset the send telegram characteristic', this.displayName);
+                self.updateValue(false);
+              }, 1000);
+            });
+        }
+        else {
+          callback();
+        }
       }
 
-      if (['Markdown', 'HTML', undefined].indexOf(this._mode) === -1) {
-        throw new Error('Invalid notification configuration. Please check your configuration of homebridge-telegram.');
-      }
-      if (!(this._notifications instanceof Array) || this._notifications.length === 0) {
-        throw new Error('Must specify at least one message for telegram notifications.');
-      }
+      pickMessage() {
+        return new Promise((resolve, reject) => {
+          try {
+            const notificationIndex = this.getMessageIndex();
+            const notification = this._activeNotifications[notificationIndex];
+            this._activeNotifications.splice(notificationIndex, 1);
 
-      this._activeNotifications = clone(this._notifications);
-      this._notificationIndex = 0;
+            if (this._activeNotifications.length == 0) {
+              this._activeNotifications = clone(this._notifications);
+            }
 
-
-      this._bot = bot;
-
-      this.on('set', this._onSet.bind(this));
-    };
-    inherits(Characteristic.SendCharacteristic, Characteristic);
-
-    Characteristic.SendCharacteristic.prototype._onSet = function (value, callback) {
-
-      const self = this;
-
-      if (value) {
-        this.pickMessage()
-          .then(message => {
-            return this._bot.send(message, this._mode);
-          })
-          .then(() => {
-            callback();
-          })
-          .catch(e => {
-            callback(e);
-          })
-          .then(() => {
-            setTimeout(() => {
-              debug('[%s] Reset the send telegram characteristic', this.displayName);
-              self.updateValue(false);
-            }, 1000);
-          });
-      }
-      else {
-        callback();
-      }
-    };
-
-    Characteristic.SendCharacteristic.prototype.pickMessage = function () {
-      return new Promise((resolve, reject) => {
-        try {
-          const notificationIndex = this.getMessageIndex();
-          const notification = this._activeNotifications[notificationIndex];
-          this._activeNotifications.splice(notificationIndex, 1);
-
-          if (this._activeNotifications.length == 0) {
-            this._activeNotifications = clone(this._notifications);
+            resolve(notification);
           }
-
-          resolve(notification);
-        }
-        catch (e) {
-          reject(e);
-        }
-      });
-    };
-
-    Characteristic.SendCharacteristic.prototype.getMessageIndex = function () {
-      if (!this._randomize) {
-        return 0;
+          catch (e) {
+            reject(e);
+          }
+        });
       }
 
-      return Math.floor(Math.random() * this._activeNotifications.length);
-    };
+      getMessageIndex() {
+        if (!this._randomize) {
+          return 0;
+        }
+
+        return Math.floor(Math.random() * this._activeNotifications.length);
+      }
+    }
+
+    Characteristic.SendCharacteristic = SendCharacteristic;
   }
 };
