ce633f40619d6cb3b4a3048b20482b747ae29be2
palasinio <31258581+palasinio@users.noreply.github.com>
2026-03-22 03:19:05 +0100

    Expose Telegram actions as standard switch services

--- src/BotAccessory.js
+++ src/BotAccessory.js
@@ -18,6 +18,8 @@ class BotAccessory {
     this.version = config.version;
 
     this._notifications = config.notifications;
+    this._notificationStates = {};
+    this._statusServices = [];
 
     this._bot = new Bot(this.name, config.token, config.chat, config.error);
     this._bot.on('connected', this._onBotConnected.bind(this));
@@ -34,10 +36,13 @@ class BotAccessory {
   }
 
   createServices() {
-    return [
-      this.getAccessoryInformationService(),
-      this.getBotService()
-    ];
+    const services = [this.getAccessoryInformationService(), this.getQuietService()];
+
+    for (const notificationName of Object.keys(this._notifications)) {
+      services.push(this.getNotificationService(notificationName));
+    }
+
+    return services;
   }
 
   getAccessoryInformationService() {
@@ -50,11 +55,14 @@ class BotAccessory {
       .setCharacteristic(Characteristic.HardwareRevision, this.version);
   }
 
-  getBotService() {
-    this._botService = new Service.TelegramBot(this.name);
-    this._botService.setCharacteristic(Characteristic.StatusActive, false);
+  getQuietService() {
+    this._quietService = new Service.Switch(`${this.name} Quiet`, 'quiet');
+    this._quietService.addOptionalCharacteristic(Characteristic.StatusActive);
+    this._quietService
+      .setCharacteristic(Characteristic.Name, 'Quiet')
+      .setCharacteristic(Characteristic.StatusActive, false);
 
-    const quietCharacteristic = this._botService.getCharacteristic(Characteristic.TelegramBotQuiet);
+    const quietCharacteristic = this._quietService.getCharacteristic(Characteristic.On);
     if (typeof quietCharacteristic.onSet === 'function') {
       quietCharacteristic.onSet(this._setQuiet.bind(this));
     }
@@ -63,13 +71,32 @@ class BotAccessory {
     }
     quietCharacteristic.updateValue(false);
 
+    this._statusServices.push(this._quietService);
 
-    for (const name of Object.keys(this._notifications)) {
-      const c = new Characteristic.SendCharacteristic(this.name, name, this._notifications[name], this._quietHandler);
-      this._botService.addCharacteristic(c);
+    return this._quietService;
+  }
+
+  getNotificationService(notificationName) {
+    const service = new Service.Switch(notificationName, `notification-${notificationName}`);
+    service.addOptionalCharacteristic(Characteristic.StatusActive);
+    service
+      .setCharacteristic(Characteristic.Name, notificationName)
+      .setCharacteristic(Characteristic.StatusActive, false);
+
+    this._notificationStates[notificationName] = this._createNotificationState(this._notifications[notificationName]);
+
+    const onCharacteristic = service.getCharacteristic(Characteristic.On);
+    if (typeof onCharacteristic.onSet === 'function') {
+      onCharacteristic.onSet(value => this._handleNotificationSet(notificationName, value));
     }
+    else {
+      onCharacteristic.on('set', (value, callback) => this._handleNotificationSet(notificationName, value, callback));
+    }
+    onCharacteristic.updateValue(false);
 
-    return this._botService;
+    this._statusServices.push(service);
+
+    return service;
   }
 
   identify(callback) {
@@ -98,9 +125,98 @@ class BotAccessory {
   }
 
   _setReachable(reachable) {
-    this._botService
-      .getCharacteristic(Characteristic.StatusActive)
-      .updateValue(reachable);
+    for (const service of this._statusServices) {
+      service
+        .getCharacteristic(Characteristic.StatusActive)
+        .updateValue(reachable);
+    }
+  }
+
+  _createNotificationState(notificationConfig) {
+    let mode;
+    let randomize;
+    let messages;
+
+    if (notificationConfig instanceof Array) {
+      mode = undefined;
+      randomize = true;
+      messages = notificationConfig;
+    }
+    else {
+      mode = notificationConfig.mode;
+      randomize = Object.prototype.hasOwnProperty.call(notificationConfig, 'randomize') ? notificationConfig.randomize : true;
+      messages = notificationConfig.messages;
+    }
+
+    if (['Markdown', 'HTML', undefined].indexOf(mode) === -1) {
+      throw new Error('Invalid notification configuration. Please check your configuration of homebridge-telegram.');
+    }
+    if (!(messages instanceof Array) || messages.length === 0) {
+      throw new Error('Must specify at least one message for telegram notifications.');
+    }
+
+    return {
+      mode: mode,
+      randomize: randomize,
+      messages: messages.slice(),
+      activeMessages: messages.slice()
+    };
+  }
+
+  _handleNotificationSet(notificationName, value, callback) {
+    const complete = typeof callback === 'function' ? callback : (() => {});
+
+    if (!value) {
+      complete();
+      return Promise.resolve();
+    }
+
+    const notificationState = this._notificationStates[notificationName];
+    const service = this._statusServices.find(currentService => currentService.getCharacteristic(Characteristic.Name).value === notificationName);
+
+    return this._pickMessage(notificationState)
+      .then(message => {
+        return this._quietHandler.send(message, notificationState.mode);
+      })
+      .then(() => {
+        complete();
+      })
+      .catch(error => {
+        complete(error);
+        throw error;
+      })
+      .finally(() => {
+        setTimeout(() => {
+          service.getCharacteristic(Characteristic.On).updateValue(false);
+        }, 1000);
+      });
+  }
+
+  _pickMessage(notificationState) {
+    return new Promise((resolve, reject) => {
+      try {
+        const notificationIndex = this._getMessageIndex(notificationState);
+        const notification = notificationState.activeMessages[notificationIndex];
+        notificationState.activeMessages.splice(notificationIndex, 1);
+
+        if (notificationState.activeMessages.length === 0) {
+          notificationState.activeMessages = notificationState.messages.slice();
+        }
+
+        resolve(notification);
+      }
+      catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  _getMessageIndex(notificationState) {
+    if (!notificationState.randomize) {
+      return 0;
+    }
+
+    return Math.floor(Math.random() * notificationState.activeMessages.length);
   }
 }
 
